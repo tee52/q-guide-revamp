20d5ec981bf0872d827c5918c310c05b75ce51c9
tee52 <onegbebi14@gmail.com>
2022-11-28 23:53:05 -0500

    created start, login, and register routes w/ layout html document and beginnings of stylesheet (basic layouts only)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
 
 import sqlite3
 
@@ -11,6 +12,28 @@ app = Flask(__name__)
 # use q-guide database
 db = sqlite3.connect("q-guide.db")
 
-@app.route("/")
-def home():
-    return "Hello world!"
+@app.route("/", methods=['GET', 'POST'])
+def start():
+    if request.method == 'POST':
+        return redirect("/login")
+    else:
+        return render_template("start.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """log user in"""
+
+    if request.method == "POST":
+        return redirect("/home")
+    else:
+        return render_template("login.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        return redirect("/home")
+
+    else:
+        return render_template("register.html")

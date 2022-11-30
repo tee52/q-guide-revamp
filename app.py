bf9e6d024487fad72869172fa93bf954fcd1a3f1
tee52 <onegbebi14@gmail.com>
2022-11-30 12:23:21 -0500

    added users table to q-guide database and added error alerts, made modifications to start screen (that don't appear on the screen), added functionality to login and register functions

--- app.py
+++ app.py
@@ -24,16 +24,66 @@ def start():
 def login():
     """log user in"""
 
+    # forget current user id, if any
+    session.clear()
+
     if request.method == "POST":
+        # check if username was entered
+        if not request.form.get("username"):
+            return render_template("login.html", error="must enter username")
+
+        # check if password was entered
+        elif not request.form.get("password"):
+            return render_template("login.html", error="must enter password")
+
+        # search database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # check if username is in database and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("login.html", error="invalid username and/or password")
+
+        # remember which user is logged in
+        session["user_id"] = rows[0]["id"]
+
+        # redirect to home page (login required)
         return redirect("/home")
+
     else:
         return render_template("login.html")
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    """register user"""
+
     if request.method == "POST":
+        usernames = db.execute("SELECT username FROM users")
+
+        # check if username was entered
+        if not request.form.get("username"):
+            return render_template("register.html", error="must enter username")
+
+        # check if password was entered
+        elif not request.form.get("password") or not request.form.get("confirmation"):
+            return render_template("register.html", error="must enter password")
+
+        # check if username is already in database
+        elif request.form.get("username") in usernames:
+            return render_template("register.html", error="username is not unique")
+
+        elif request.form.get("confirmation") != request.form.get("password"):
+            return render_template("register.html", error="passwords do not match")
+
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8))
+
         return redirect("/home")
 
     else:
         return render_template("register.html")
+
+
+@app.route("/home")
+@login_required
+def home():
+    return render_template("home.html")
